58f4cca1536c2ddb62446da46afd7522280be462
diego-burgos <64620373+diego-burgos@users.noreply.github.com>
2022-04-27 11:10:31 -0500

    Add files via upload

---
+++ ex-par1-dad/src/main/java/ejercicio3/Utilidades.java
@@ -0,0 +1,26 @@
+package ejercicio3;
+
+import java.util.Random;
+
+public class Utilidades {
+    public static void numAzar(String[] args) {
+                int numAzar;
+		int min = 1;
+		int max = 10;
+
+		Random random = new Random();
+
+		numAzar = random.nextInt(max + min) + min;
+                
+    }
+
+    static int numAzar(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void esperarTiempoAzar(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+}
